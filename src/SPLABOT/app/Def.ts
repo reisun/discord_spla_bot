e052a9ce2ee4fd351c3511479f270b44840935cb
rrrrrrr <r_nishikawa_renraku@outlook.jp>
2023-10-17 01:47:46 +0900

    アルファベットが上限より少なかったので追加

--- src/SPLABOT/app/Def.ts
+++ src/SPLABOT/app/Def.ts
@@ -1,5 +1,5 @@
 export const ALPHABET_TABLE = [
-    "A", "B", "F", "H", "W", "X", "Y", "Z", "N", "L", "Q", "S",
+    "A", "B", "F", "H", "W", "X", "Y", "Z", "N", "L", "Q", "S", "R", "I"
 ] as const;
 
 export const MAX_MEMBER_COUNT: number = 14;
